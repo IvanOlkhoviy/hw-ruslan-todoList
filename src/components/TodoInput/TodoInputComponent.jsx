6f9bec27190c53134e1071e0bf51f060b5f51870
IvanOlkhoviy <ivanolkhoviy@gmail.com>
2020-10-31 21:02:05 +0200

    Fixed gutter bug

--- src/components/TodoInput/TodoInputComponent.jsx
+++ src/components/TodoInput/TodoInputComponent.jsx
@@ -5,7 +5,7 @@ function TodoInputComponent({inputValue,changeInputValue,addTodo}) {
 
 
     return (
-        <div className = "row inputContainer justify-content-center mb-5">
+        <div className = "row inputContainer justify-content-center no-gutters mb-5">
             <input className = "col-4 mr-3" placeholder = "Please enter what you have to do"
             onChange = {(e) => changeInputValue(e.target.value)}
             value = {inputValue}>
--- src/components/TodoList/TodoListComponent.jsx
+++ src/components/TodoList/TodoListComponent.jsx
@@ -4,7 +4,7 @@ import "./ListStyle.css"
 function TodoListComponent({id, todoTitle, todoDelete}) {
     
     return(
-        <div className = "row justify-content-center mb-3 ">
+        <div className = "row justify-content-center no-gutters mb-3 ">
             <p className = "col-5 text-left item">{todoTitle}</p>
             <button id = {id} className = "btn btn-danger"
              onClick = { (e) => todoDelete(e.target.id) }>
